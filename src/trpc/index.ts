465801135d8bbae84610a733c4730417ffbd3757
Mahmud Lukman <mistamann49@gmail.com>
2023-11-20 14:09:22 +0100

    auth-callback error fixed

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,4 +1,4 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import {
   publicProcedure,
   router,
@@ -9,10 +9,10 @@ import { db } from '@/db'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
-    const { getUser } = getKindeServerSession()
-    const user = getUser() 
+    const {getUser} = getKindeServerSession()
+    const user = await getUser()
 
-    if (!user.id || !user.email)
+    if (!user?.id || !user?.email)
       throw new TRPCError({ code: 'UNAUTHORIZED' })
 
     // check if the user is in the database
