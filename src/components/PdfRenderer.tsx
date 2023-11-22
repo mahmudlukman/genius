7af5fba817f1b99be2ef47559f61d3bf9cfa1984
Mahmud Lukman <mistamann49@gmail.com>
2023-11-22 16:10:35 +0100

    switch page functionality added in PDFrenderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,49 +1,87 @@
-"use client"
+'use client';
 
-import { Loader2 } from 'lucide-react'
-import {Document, Page, pdfjs} from 'react-pdf'
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
-import { useToast } from './ui/use-toast'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import { useToast } from './ui/use-toast';
+import { useResizeDetector } from 'react-resize-detector';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { useState } from 'react';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
-  url: string
+  url: string;
 }
 
-const PdfRenderer = ({url}: PdfRendererProps) => {
-  const {toast} = useToast()
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const [numPages, setNumPages] = useState<number>()
+  const [currPage, setCurrPage] = useState<number>(1)
+  const { width, ref } = useResizeDetector();
   return (
-    <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
-          Top Bar
+          <Button 
+          disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+            }}
+            variant='ghost' 
+            aria-label='previous page'>
+            <ChevronDown className='h-4 w-4'/>
+          </Button>
+
+          <div className="flex items-center gap-1.5">
+            <Input className='w-12 h-8'/>
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? 'x'}</span>
+            </p>
+          </div>
+
+          <Button 
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) => (prev + 1 > numPages! ? numPages! : prev + 1))
+            }}
+            variant='ghost' 
+            aria-label='next page'>
+            <ChevronUp className='h-4 w-4'/>
+          </Button>
         </div>
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <div>
-          <Document 
+        <div ref={ref}>
+          <Document
             loading={
-              <div className='flex justify-center'>
-                <Loader2 className='my-24 h-6 w-6 animate-spin'/>
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
               </div>
             }
             onLoadError={() => {
               toast({
                 title: 'Error loading PDF',
                 description: 'Please try again later',
-                variant: 'destructive'
-              })
+                variant: 'destructive',
+              });
             }}
+            onLoadSuccess={({numPages}) => setNumPages(numPages)}
             file={url}
-            className='max-h-full'>
-            <Page pageNumber={1}/>
+            className="max-h-full"
+          >
+            <Page 
+              // width={width ? width : 1} 
+              pageNumber={currPage} />
           </Document>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PdfRenderer
+export default PdfRenderer;
