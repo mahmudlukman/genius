73eacd21a720bafbc83e5ad1c09e2f37308fdef5
Mahmud Lukman <mistamann49@gmail.com>
2023-11-22 12:52:32 +0100

    file upload complete

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -12,72 +12,72 @@ import { trpc } from '@/app/_trpc/client';
 import { useRouter } from 'next/navigation';
 
 const UploadDropzone = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [isUploading, setIsUploading] = useState<boolean>(false)
-  const [uploadProgress, setUploadProgress] = useState<number>(0)
-  const {toast} = useToast()
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const { toast } = useToast();
 
-  const {startUpload} = useUploadThing('pdfUploader')
+  const { startUpload } = useUploadThing('pdfUploader');
 
-  const {mutate: startPolling} = trpc.getFile.useMutation({
+  const { mutate: startPolling } = trpc.getFile.useMutation({
     onSuccess: (file) => {
-      router.push(`/dashboard/${file.id}`)
+      router.push(`/dashboard/${file.id}`);
     },
     retry: true,
-    retryDelay: 500
-  })
+    retryDelay: 500,
+  });
 
-  const startSimulatedProgress =  () => {
-    setUploadProgress(0)
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
 
     const interval = setInterval(() => {
       setUploadProgress((prevProgress) => {
-        if(prevProgress >= 95) {
-          clearInterval(interval)
-          return prevProgress
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
         }
-        return prevProgress + 5
-      })
-    }, 500)
-    return interval
-  }
+        return prevProgress + 5;
+      });
+    }, 500);
+    return interval;
+  };
 
   return (
     <Dropzone
       multiple={false}
-      onDrop={async(acceptedFile) => {
-        setIsUploading(true)
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
 
-        const progressInterval = startSimulatedProgress()
+        const progressInterval = startSimulatedProgress();
 
         // handle file uploading
-        const res = await startUpload(acceptedFile)
+        const res = await startUpload(acceptedFile);
 
-        if(!res) {
+        if (!res) {
           return toast({
             title: 'Something went wrong',
             description: 'Please try again later',
-            variant: 'destructive'
-          })
+            variant: 'destructive',
+          });
         }
 
-        const [fileResponse] = res
+        const [fileResponse] = res;
 
-        const key = fileResponse?.key
+        const key = fileResponse?.key;
 
-        if(!key) {
+        if (!key) {
           return toast({
             title: 'Something went wrong',
             description: 'Please try again later',
-            variant: 'destructive'
-          })
+            variant: 'destructive',
+          });
         }
 
-        clearInterval(progressInterval)
-        setUploadProgress(100)
+        clearInterval(progressInterval);
+        setUploadProgress(100);
 
-        startPolling({key})
+        startPolling({ key });
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -102,7 +102,7 @@ const UploadDropzone = () => {
               {acceptedFiles && acceptedFiles[0] ? (
                 <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
                   <div className="px-3 py-2 h-full grid place-items-center">
-                    <File className='h-4 w-4 text-blue-500'/>
+                    <File className="h-4 w-4 text-blue-500" />
                   </div>
                   <div className="px-3 py-2 h-full text-sm truncate">
                     {acceptedFiles[0].name}
@@ -110,16 +110,29 @@ const UploadDropzone = () => {
                 </div>
               ) : null}
 
-              {isUploading ? (<div className='w-full mt-4 max-w-xs mx-auto'>
-                <Progress value={uploadProgress} className='h-1 w-full big-zinc-200'/>
-              </div>) : null}
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress
+                    indicatorColor={
+                      uploadProgress === 100 ? 'bg-green-500' : ''
+                    }
+                    value={uploadProgress}
+                    className="h-1 w-full big-zinc-200"
+                  />
+                </div>
+              ) : null}
               {uploadProgress === 100 ? (
-                <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
-                  <Loader2 className='h-3 w-3 animate-spin'/>
+                <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                  <Loader2 className="h-3 w-3 animate-spin" />
                   Redirecting...
                 </div>
               ) : null}
-              <input {...getInputProps()} type="file" id='dropzone-file' className='hidden'/>
+              <input
+                {...getInputProps()}
+                type="file"
+                id="dropzone-file"
+                className="hidden"
+              />
             </label>
           </div>
         </div>
