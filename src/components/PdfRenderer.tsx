5dbdb421f47f952fe8f27d3a217c6374a490f832
Mahmud Lukman <mistamann49@gmail.com>
2023-11-23 15:52:02 +0100

    pdf rotate functionality added

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChevronDown, ChevronUp, Loader2, Search } from 'lucide-react';
+import { ChevronDown, ChevronUp, Loader2, RotateCw, Search } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -33,6 +33,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
   const [numPages, setNumPages] = useState<number>();
   const [currPage, setCurrPage] = useState<number>(1);
   const [scale, setScale] = useState<number>(1);
+  const [rotation, setRotation] = useState<number>(0);
 
   const CustomPageValidator = z.object({
     page: z
@@ -132,6 +133,13 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+
+          <Button 
+            onClick={() => setRotation((prev) => prev + 90)}
+            variant='ghost' 
+            aria-label='rotate 90 degrees'>
+            <RotateCw className='h-4 w-4'/>
+          </Button>
         </div>
       </div>
 
@@ -159,6 +167,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                 // width={width ? width : 1}
                 pageNumber={currPage}
                 scale={scale}
+                rotate={rotation}
               />
             </Document>
           </div>
