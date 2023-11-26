1856ab251898463a0142d3f1c94061ba5a146269
Mahmud Lukman <mistamann49@gmail.com>
2023-11-26 17:26:14 +0100

    chat context added

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -22,35 +22,35 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     }
   );
 
-  if (isLoading)
-    return (
-      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-        <div className="flex-1 flex justify-center items-center flex-col mb-28">
-          <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
-            <h3 className="font-semibold text-xl">Loading...</h3>
-            <p className="text-zinc-500 text-sm">
-              Genius is preparing your PDF
-            </p>
-          </div>
-        </div>
-      </div>
-    );
+  // if (isLoading)
+  //   return (
+  //     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+  //       <div className="flex-1 flex justify-center items-center flex-col mb-28">
+  //         <div className="flex flex-col items-center gap-2">
+  //           <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+  //           <h3 className="font-semibold text-xl">Loading...</h3>
+  //           <p className="text-zinc-500 text-sm">
+  //             Genius is preparing your PDF
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 
-  if (data?.status === 'PROCESSING')
-    return (
-      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-        <div className="flex-1 flex justify-center items-center flex-col mb-28">
-          <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
-            <h3 className="font-semibold text-xl">Processing PDF...</h3>
-            <p className="text-zinc-500 text-sm">
-              Genius is processing your PDF
-            </p>
-          </div>
-        </div>
-      </div>
-    );
+  // if (data?.status === 'PROCESSING')
+  //   return (
+  //     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+  //       <div className="flex-1 flex justify-center items-center flex-col mb-28">
+  //         <div className="flex flex-col items-center gap-2">
+  //           <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+  //           <h3 className="font-semibold text-xl">Processing PDF...</h3>
+  //           <p className="text-zinc-500 text-sm">
+  //             Genius is processing your PDF
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 
   if (data?.status === 'FAILED')
     return (
